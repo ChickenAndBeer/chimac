92eca8b6c10e76a81a93318a5dc3bd9d859c7eb6
Claudia Baik <claudiabaik@gmail.com>
2018-03-08 12:08:54 -0500

    finished express

--- server/index.js
+++ server/index.js
@@ -1,3 +1,85 @@
-const db= require('./db');
+const express = require("express");
+const app = express();
+const db = require("./db");
+const morgan = require("morgan");
+const path = require("path");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sessionStore = new SequelizeStore({ db });
+const PORT = process.env.PORT || 8080
+// if (process.env.NODE_ENV !== 'production') require('../secrets')
 
-db.sync({force: true}).then(()=> console.log('SYNCEDDDDDD'))
+//LOGGING MIDDLEWARE
+app.use(morgan);
+
+//BODY PARSING MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// // compression middleware
+// app.use(compression())
+
+//EXPRESS SESSION
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "chimac is so yum",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//app.use("/auth", require("./auth"));
+//app.use("/api", require("./api"));
+
+app.use((req, res, next) => {
+  console.log("SESSION: ", req.session);
+  next();
+});
+
+//STATIC FILE_SERVING MIDDLEWARE
+app.use(express.static(path.join(__dirname, "...", "public")));
+
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+});
+
+//ERROR HANDLING
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal Error");
+});
+
+//PASSPORT
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) =>
+  db.models.user
+    .findById(id)
+    .then(user => done(null, user))
+    .catch(done)
+);
+
+db.sync({ force: true })
+  .then(() => {
+    console.log('Postgres running and your data is SYNCED')
+    app.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`Your sever is listening on ${PORT}`)
+    })
+  })
+  .catch(console.error)
+
